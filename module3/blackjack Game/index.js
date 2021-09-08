14660e9ffcce160a1e02e5c84e01bb0d5c85fddc
Nairita Paul <nairitapaul10@gmail.com>
2021-09-08 17:17:35 +0600

    assign values in the startGame() function

--- module3/blackjack Game/index.js
+++ module3/blackjack Game/index.js
@@ -1,9 +1,5 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard] // array - ordered list of items
-let sum = firstCard + secondCard
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -21,6 +17,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
