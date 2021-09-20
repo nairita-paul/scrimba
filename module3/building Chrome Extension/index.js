fc690b6a6a7d75f5ecd794be38e8e5a56b3a5166
Nairita Paul <nairitapaul10@gmail.com>
2021-09-20 17:07:04 +0600

    refactor the app to use a template String

--- module3/building Chrome Extension/index.js
+++ module3/building Chrome Extension/index.js
@@ -5,12 +5,21 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
+
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li> "
+        // Refactor the code below to use a template string
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
