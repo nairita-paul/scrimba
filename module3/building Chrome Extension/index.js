a68db2d82e7e1c04e319b67e625950956a4dee22
Nairita Paul <nairitapaul10@gmail.com>
2021-09-28 19:36:17 +0600

    make the delete button work

--- module3/building Chrome Extension/index.js
+++ module3/building Chrome Extension/index.js
@@ -2,7 +2,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
+const deleteBtn = document.getElementById("delete-btn");
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
 localStorage.clear();
@@ -11,6 +11,12 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -22,7 +28,6 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // Refactor the code below to use a template string
     listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
