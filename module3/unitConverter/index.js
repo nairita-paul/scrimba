fdf0894cd6f8f5abbaa0732cc0fe0da129ab26f2
Nairita Paul <nairitapaul10@gmail.com>
2022-06-17 17:37:28 +0800

    completed building unitConverter Project

---
+++ module3/unitConverter/index.js
@@ -0,0 +1,44 @@
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const length = document.getElementById("length");
+const volumn = document.getElementById("volumn");
+const mass = document.getElementById("mass");
+let meterToFeet = 3.281;
+let litterToGallons = 0.264;
+let kilogramToPound = 2.204;
+
+function unitConverter() {
+  convertLength();
+  convertVolumn();
+  convertMass();
+}
+
+function convertLength() {
+  let basevalue = input.value;
+  length.textContent = ` 
+  ${basevalue} meter = ${
+    basevalue * meterToFeet
+  } feet  | ${basevalue} feet = ${(input.value / meterToFeet).toFixed(
+    2
+  )} meter  `;
+}
+
+function convertVolumn() {
+  let basevalue = input.value;
+  volumn.textContent = ` ${input.value} litter = ${
+    basevalue * litterToGallons
+  } gallon | ${basevalue} gallon = ${(input.value / litterToGallons).toFixed(
+    2
+  )} meter  `;
+}
+
+function convertMass() {
+  let basevalue = input.value;
+  mass.textContent = ` ${input.value} kilo = ${(
+    basevalue * kilogramToPound
+  ).toFixed(2)} pound | ${basevalue} pound = ${(
+    input.value / kilogramToPound
+  ).toFixed(2)} meter `;
+}
+
+unitConverter();
