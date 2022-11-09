b218048a12b3ea40fa7d477bce13db6c2f07e908
Nairita Paul <nairitapaul10@gmail.com>
2022-11-09 17:06:27 +0800

    Refactor and dynamically adds posts

--- soloProjects/instragramClone/index.js
+++ soloProjects/instragramClone/index.js
@@ -28,3 +28,45 @@ const posts = [
     likes: 152,
   },
 ];
+
+function render() {
+  let html = `
+
+  <header class="header-container">
+      <div class="flex-author-info">
+        <img class="insta-avatar" src="images/logo.png" />
+        <img class="user-avatar" src="images/user-avatar1.jpg" />
+      </div>
+  </header> 
+  `;
+  posts.forEach(function (item) {
+    const { name, username, location, avatar, post, comment, likes } = item;
+
+    html += `
+
+  <section>
+    <div class="first-section flex-col">
+      <img class="user-avatar" src="${avatar}" />
+      <div>
+        <p class="author-name">${name}</p>
+        <p class="author-location">${location}</p>
+      </div>
+    </div>
+    <img class="post-pic" src="${post}" />
+    <div class="flex-icon">
+      <img class="icon" src="images/icon-heart.png" />
+      <img class="icon" src="images/icon-comment.png" />
+      <img class="icon" src="images/icon-dm.png" />
+    </div>
+    <p class="likes">${likes}    likes</p>
+    <div>
+      <p class="post-desc">
+        <strong> ${username}</strong> ${comment}
+      </p>
+    </div>
+  </section>
+        `;
+  });
+  document.body.innerHTML = html;
+}
+render();
