2ffa78911f15f129805842fa6b03745a17f017a9
Nairita Paul <nairitapaul10@gmail.com>
2023-02-01 16:51:43 +0800

    adds card badge to the card section

--- soloProjects/airBnb/src/Card/Card.jsx
+++ soloProjects/airBnb/src/Card/Card.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./Card.css";
 import star from "../assets/Star.png";
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={props.img} className="card--image" />
       <div className="card--stats">
         <img src={star} className="card--star" />
