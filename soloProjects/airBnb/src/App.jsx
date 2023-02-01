2190cc6d104b3fe585f2b0b49699561b58e5582c
Nairita Paul <nairitapaul10@gmail.com>
2023-02-01 16:20:20 +0800

    completes the card section

--- soloProjects/airBnb/src/App.jsx
+++ soloProjects/airBnb/src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import Card from "./Card/Card";
+import data from "./data";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
-    <div>
+    <div class="rity">
       <Navbar />
       <Hero />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
