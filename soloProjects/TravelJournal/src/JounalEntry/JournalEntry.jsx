cdf9886ac8abe55731845a34469512018339283e
Nairita Paul <nairitapaul10@gmail.com>
2023-02-09 01:41:33 +0800

    [TJ] refactor journal entry to remove extra div

--- soloProjects/TravelJournal/src/JounalEntry/JournalEntry.jsx
+++ soloProjects/TravelJournal/src/JounalEntry/JournalEntry.jsx
@@ -3,21 +3,18 @@ import "./JournalEntry.css";
 
 export default function JournalEntry(props) {
   return (
-    <div className="section">
-      <div className="entry">
-        <img src={props.imageUrl} />
-        <div className="information">
-          <div className="location-meta">
-            <img className="location-pin" src={props.locationPinImage}></img>
-            <p className="country">{props.country}</p>
-            <p className="map-link">{props.googleMapsUrl}</p>
-          </div>
-          <h1 className="title">{props.title}</h1>
-          <p className="date">{props.date}</p>
-          <p className="description">{props.description}</p>
+    <div className="entry">
+      <img src={props.imageUrl} />
+      <div className="information">
+        <div className="location-meta">
+          <img className="location-pin" src={props.locationPinImage}></img>
+          <p className="country">{props.country}</p>
+          <p className="map-link">{props.googleMapsUrl}</p>
         </div>
+        <h1 className="title">{props.title}</h1>
+        <p className="date">{props.date}</p>
+        <p className="description">{props.description}</p>
       </div>
-      <hr></hr>
     </div>
   );
 }
